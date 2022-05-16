4f1eded9a4a79e036d966d988bf3aa290f37f5b9
“Srijan-Chak012” <srijan.chakraborty@research.iiit.ac.in>
2022-05-16 20:18:00 +0530

    Models done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const app = express();
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
+// Load env vars
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log('Connected to MongoDB Atlas');
+});
+
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+    console.log('Server is running on port 8800');
+});
